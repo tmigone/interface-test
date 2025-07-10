70fb5f6820bb5d2dad43f245bc26f0baecccada2
Tomás Migone <tomas@edgeandnode.com>
2025-07-10 15:28:26 -0300

    chore: add example with ethers-v5
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { createConfig, http, readContract } from '@wagmi/core'
 import { mainnet } from '@wagmi/core/chains'
 import { l2GraphTokenAbi } from '@graphprotocol/interfaces/wagmi'
+import { IL2GraphToken__factory } from '@graphprotocol/interfaces/types-v5'
 
 const config = createConfig({
   chains: [mainnet],
@@ -11,6 +12,7 @@ const config = createConfig({
 
 
 async function main() {
+  // wagmi
   const result = await readContract(
     config, {
     address: '0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2',
@@ -20,6 +22,11 @@ async function main() {
   });
 
   console.log(result);
+
+  // ethers-v5
+  const l2GraphToken = IL2GraphToken__factory.connect('0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2', config.chains[0].rpcUrls.default.http[0])
+  const result2 = await l2GraphToken.balanceOf('0x0000000000000000000000000000000000000000')
+  console.log(result2)
 }
 
 main();
